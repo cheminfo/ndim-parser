60c995ca4a6b5413d28b852db048f1aec824433b
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-11-26 10:58:41 +0100

    feat: initial implemetation

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,7 +1,24 @@
-import { myModule } from '..';
+import { ndParse } from '..';
 
-describe('test myModule', () => {
-  it('should return 42', () => {
-    expect(myModule()).toBe(42);
+const simpleExample = `
+meta.first,1
+meta.second,,2
+temp, volt, curr
+1,1,1
+2,2,2
+3,3,3
+`;
+
+test('simpleExample', () => {
+  expect(ndParse(simpleExample)).toStrictEqual({
+    data: {
+      temp: { data: [1, 2, 3], label: 'temp' },
+      volt: { data: [1, 2, 3], label: 'volt' },
+      curr: { data: [1, 2, 3], label: 'curr' },
+    },
+    meta: {
+      'meta.first': '1',
+      'meta.second': '2',
+    },
   });
 });
